4675885d5e1c3045085d5315a0b58a7d56131e53
Olatunbosun Oladayo Micheal <tubosund@gmail.com>
2023-02-16 07:21:02 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-printf("size of a char: %lu bytes\n", sizeof(a));
-printf("size of a int: %lu bytes\n", sizeof(b));
-printf("size of a long int: %lu bytes\n", sizeof(c));
-printf("size of a long long int: %lu bytes\n", sizeof(d));
-printf("size of a float: %lu bytes\n", sizeof(f));
+printf("size of a char: %lu byte(s)\n", sizeof(a));
+printf("size of a int: %lu byte(s)\n", sizeof(b));
+printf("size of a long int: %lu byte(s)\n", sizeof(c));
+printf("size of a long long int: %lu byte(s)\n", sizeof(d));
+printf("size of a float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
