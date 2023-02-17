2a0524a901775b2d5fdb151aae78ddcf2c7cf127
Olatunbosun Oladayo Micheal <tubosund@gmail.com>
2023-02-17 11:17:13 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar (alp);
+	putchar('\n');
+	return (0);
+}
