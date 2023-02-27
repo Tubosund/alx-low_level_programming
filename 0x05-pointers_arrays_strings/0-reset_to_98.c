5fc1dbc992064c1e3db14ec080b717bfe538e441
Olatunbosun Oladayo Micheal <tubosund@gmail.com>
2023-02-27 03:17:36 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value of a variable
+ * The pointer points to 98
+ * @n: pointer to the variable to update
+ */
+
+void reset_to_98(int *n)
+{
+	int i = 98;
+	*n = &i
+
+	* n = 98;
+}
