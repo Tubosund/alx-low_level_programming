49aaf5741dc4a7a661d4fc85cb98d4f92f525850
Olatunbosun Oladayo <tubosund@gmail.com>
2023-05-04 16:11:48 -0700

    .

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,6 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * CHAR_BIT)
 		return (-1);
-	unsigned long int mask = 1UL << index;
 
-	return ((n & mask) != 0);
+	return ((n >> index) & 1);
 }
